8550f9cb652a68bc933d46933615bca336bcbafb
pamelafawaz <144020030+pamelafawaz@users.noreply.github.com>
2023-11-09 21:22:03 +0100

    Create winefraud_app.py

---
+++ winefraud_app.py
@@ -0,0 +1,7 @@
+echo "# Wine_Fraud_Detection_Streamlit_Pamela" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/pamelafawaz/Wine_Fraud_Detection_Streamlit_Pamela.git
+git push -u origin main
